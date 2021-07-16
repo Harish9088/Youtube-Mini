e8a7be15c4262e48e6d800cbc54909232bd5bf04
Harish9088 <harishlycan9088@gmail.com>
2021-07-16 17:35:54 +0530

    Update youtube.js

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,11 +1,12 @@
 import axios from "axios";
-const KEY="AIzaSyDUx6uypCwVz6FMPwroL5twGPlfk9pZzaY"
+
+const KEY=process.env.REACT_APP_KEY
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
     
         params:{
            part:"snippet",
-            maxResult:1,
+            maxResult:10,
         key:KEY
         
         
