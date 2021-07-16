43a4dccc1e6346fc24b4b8fe419e4fc08476a59c
Harish9088 <harishlycan9088@gmail.com>
2021-07-16 17:27:10 +0530

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,22 @@
-import React,{useState} from 'react'
+import React from 'react'
+import {IoMdSearch} from "react-icons/io"
 
-export default function SearchBar({handleSubmit,handleChange,input}) {
+export default function SearchBar({handleSubmit,handleChange,input,handleClick}) {
  
   
   return (
     <div>
       <form onSubmit={handleSubmit} >
-        <input type="text" placeholder="Search" name={input} value={input} onChange={handleChange}/>
-        <button>Search</button>
+        <input 
+        type="text" 
+        placeholder="Search" 
+        name={input} 
+        value={input} 
+        onChange={handleChange}
+        onKeyPress={handleClick}
+          className="searchBar"
+        />
+        <button className="search-btn"><IoMdSearch className="search-icon"/>Search</button>
       </form>
     </div>
   )
