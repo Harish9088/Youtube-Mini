2eccca0c83cd1bb80f6fb69d5409433a74006894
Harish9088 <harishlycan9088@gmail.com>
2021-07-26 23:31:01 +0530

    Update VideoList.js

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,22 +3,20 @@ import VideoItem from "./VideoItem"
 
 export default function VideoList({videos,videoSelect}) {
   return (
-    <div className="videoList" >
+    <section className="videoList" >
        {videos.map(video=>
        {
-            return <div className="videoListItem" >
+            return <div  key={video.id.videoId} >
       <VideoItem 
       videoSelect={videoSelect}
       image={video.snippet.thumbnails.medium.url} 
       id={video.id.videoId} 
       title={video.snippet.title} 
       channel={video.snippet.channelTitle}
-      
-      />
-      
-          </div>
+       />
+       </div>
        })}
 
-    </div>
+    </section>
   )
 }
