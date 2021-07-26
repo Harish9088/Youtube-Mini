6194a654fc50b7c83a6b2f2edca742daf9429fcb
Harish9088 <harishlycan9088@gmail.com>
2021-07-26 23:29:10 +0530

    Update VideoDetail.js

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,13 +2,11 @@ import React from 'react'
 
 function VideoDetail({src}) {
  return (
-    <div className="videoDetail">
-      <iframe width="750px" height="450px" src={src.link} title={src.title} channel={src.channel}/>
-      <div className="videoText">
-      <h2>{src.title}</h2>
+    <section className="videoDetail">
+      <iframe className="iFrame" width="750px" height="450px" src={src.link} title={src.title} channel={src.channel}/>
+      <h2 className="videoTitle">{src.title}</h2>
       <p >{src.channel}</p>
-      </div>
-    </div>
+    </section>
   )
 }
 
